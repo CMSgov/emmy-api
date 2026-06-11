aff99bc1fc83a61482395b2c79d3566ad81437c8
Neil MartinsenBurrell <neilmartinsenburrell@gmail.com>
2026-06-11 12:09:07 -0500

    [FFS-4468] Add schoolCode (#92)
    * Add schoolCode to example payloads
    
    * NSC API returns schoolCode as a string
    
    * Modify distributed version of the api spec

--- rails-app/spec/services/education/nsc_client_spec.rb
+++ rails-app/spec/services/education/nsc_client_spec.rb
@@ -71,6 +71,7 @@ module Education
           enrollmentDetails: [ {
             currentEnrollmentStatus: 'CC',
             officialSchoolName: 'University A',
+            schoolCode: 001171,
             enrollmentData: [ { enrollmentStatus: 'H', termBeginDate: '2023-01-01', termEndDate: '2023-05-01' } ]
           } ]
         }.to_json)
